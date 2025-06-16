a5dae697bd9e72f8ddb92c607ca594744ad5ea8f
rajat-ws <rajat.mehra@wednesday.is>
2025-06-16 13:34:51 +0530

    fix: prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
 import js from "@eslint/js";
-import globals from "globals";
+import prettierConfig from "eslint-config-prettier";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -26,7 +30,5 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": "off",
     },
-  },
-  // Add Prettier config last to override formatting rules
-  prettierConfig,
+  }
 );
